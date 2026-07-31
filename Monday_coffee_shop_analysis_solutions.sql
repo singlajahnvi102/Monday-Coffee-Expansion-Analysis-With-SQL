6ba0be069245b506d8d1ddc167312da6b7179f92
singlajahnvi102 <singlajahnvi102@gmail.com>
2026-07-31 22:19:31 +0530

    Add files via upload

---
+++ Monday_coffee_shop_analysis_solutions.sql
@@ -0,0 +1,35 @@
+create database coffee_shop;
+use   coffee_shop;
+CREATE TABLE city (
+    city_id INT PRIMARY KEY,
+    city_name VARCHAR(15),
+    population BIGINT,
+    estimated_rent FLOAT,
+    city_rank INT
+);
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(50),
+    city_id INT,
+    CONSTRAINT fk_city FOREIGN KEY (city_id)
+        REFERENCES city (city_id)
+);
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    price FLOAT
+);
+CREATE TABLE sales (
+    sale_id INT PRIMARY KEY,
+    sale_date DATE,
+    product_id INT,
+    customer_id INT,
+    total FLOAT,
+    rating INT,
+    CONSTRAINT fk_products FOREIGN KEY (product_id)
+        REFERENCES products (product_id),
+    CONSTRAINT fk_customers FOREIGN KEY (customer_id)
+        REFERENCES customers (customer_id)
+); 
+select * from sales;
+select * from city;
